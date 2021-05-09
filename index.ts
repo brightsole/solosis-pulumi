523c3b66185aca1874529b995516697935c7a1a3
119 <10665739+one19@users.noreply.github.com>
2021-05-09 18:19:07 +1000

    :wrench: add pulumi files needed to launch it.

---
+++ index.ts
@@ -0,0 +1,60 @@
+import * as aws from '@pulumi/aws';
+import * as awsx from '@pulumi/awsx';
+import * as pulumi from '@pulumi/pulumi';
+import callbackFactory from './src/server';
+
+const stackConfig = new pulumi.Config();
+
+const config = {
+  tableName: stackConfig.require('tableName'),
+  token: stackConfig.requireSecret('token'),
+  region: stackConfig.require('region'),
+};
+
+// create a table to point at
+new aws.dynamodb.Table(config.tableName, {
+  name: config.tableName,
+  attributes: [
+    {
+      name: 'id',
+      type: 'S',
+    },
+  ],
+  hashKey: 'id',
+  readCapacity: 5,
+  writeCapacity: 5,
+});
+
+// Create an API endpoint
+const endpoint = new awsx.apigateway.API('solosis', {
+  routes: [
+    {
+      path: '/graphql',
+      method: 'ANY',
+      eventHandler: new aws.lambda.CallbackFunction('solosis', {
+        // set this high because low memory often costs more with long times
+        memorySize: 1024,
+        environment: {
+          variables: {
+            TOKEN: config.token,
+            REGION: config.region,
+            TABLE_NAME: config.tableName,
+          },
+        },
+        policies: [
+          // able to be executed
+          aws.iam.ManagedPolicies.AWSLambdaVPCAccessExecutionRole,
+          // need to be able to create and modify the function
+          aws.iam.ManagedPolicy.LambdaFullAccess,
+          // this function needs permissions to access that database
+          aws.iam.ManagedPolicy.AmazonDynamoDBFullAccess,
+          // to enable complex functionality, we need access to event bridge emissions
+          'arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess',
+        ],
+        callbackFactory,
+      } as any),
+    },
+  ],
+});
+
+exports.endpoint = endpoint.url;
